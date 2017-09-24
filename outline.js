7cecd33f12c67d3974ca6346924228436c0a261f
ademcifci <ademciftcioglu@gmail.com>
2017-09-24 17:46:32 +1000

    Update outline.js

--- outline.js
+++ outline.js
@@ -7,7 +7,7 @@
     for (var i = 0, n = all_el.length; i < n; i++) {
         if (/^h\d{1}$/gi.test(all_el[i].nodeName)) {
             //arr.push(all_el[i].nodeName);
-		var dsp = all_el[i].currentStyle.getPropertyValue('display');
+		var dsp = all_el[i].currentStyle ? all_el[i].currentStyle.display : getComputedStyle(all_el[i], null).display;
 			if(dsp  == 'none')
 				{
 					a += all_el[i].nodeName+" - "+all_el[i].innerHTML+ " (display:none)<br />\n";
